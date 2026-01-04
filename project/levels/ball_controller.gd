e11933532e6fd8a3e697c8cc21c40bc42a94dbb4
Jadi Miller <jadilmiller@gmail.com>
2026-01-04 18:10:29 -0500

    Aim and adjust power of Ball using arrow keys

--- project/levels/ball_controller.gd
+++ project/levels/ball_controller.gd
@@ -3,7 +3,18 @@ extends Node
 
 @export var _ball: Ball
 
+const POWER_ADJUST_AMOUNT: float = 0.05
+const TURN_AMOUNT: float = 0.1
+
 
 func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("increase_power"):
+		_ball.adjust_power(POWER_ADJUST_AMOUNT)
+	if event.is_action_pressed("decrease_power"):
+		_ball.adjust_power(-POWER_ADJUST_AMOUNT)
+	if event.is_action_pressed("rotate_left"):
+		_ball.turn(TURN_AMOUNT)
+	if event.is_action_pressed("rotate_right"):
+		_ball.turn(-TURN_AMOUNT)
 	if event.is_action_pressed("hit"):
 		_ball.hit()
