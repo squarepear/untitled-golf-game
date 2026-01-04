02b2da8bde91295cc226edf408e08007fc9f1875
Jeffrey Harmon <contact@jeffreyharmon.dev>
2026-01-04 17:54:22 -0500

    Add ball control methods

--- project/golfer/ball/ball.gd
+++ project/golfer/ball/ball.gd
@@ -1,5 +1,28 @@
+class_name Ball
 extends RigidBody3D
 
-func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		apply_impulse(Vector3(3, 0, 0))
+const MAX_POWER := 10.0
+
+var _power_percentage := 0.5
+
+@onready var _aimer: Node3D = %Aimer
+@onready var _pivot: Node3D = %Pivot
+@onready var _power_indicator: MeshInstance3D = %PowerIndicator
+
+
+func _process(_delta: float) -> void:
+	_aimer.global_position = global_position
+
+
+func hit() -> void:
+	apply_impulse(-_pivot.basis.z * _power_percentage * MAX_POWER)
+
+
+func turn(angle: float) -> void:
+	_pivot.rotate_y(angle)
+
+
+func adjust_power(amount: float) -> void:
+	# TODO: Add safeguards
+	_power_percentage += amount
+	_power_indicator.mesh.size.y = _power_percentage
