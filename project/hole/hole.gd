00b41a876f7d06aac040b6c496b5500b8b7a6616
Jeffrey Harmon <contact@jeffreyharmon.dev>
2026-01-04 19:03:02 -0500

    Add ball detection to hole

--- project/hole/hole.gd
+++ project/hole/hole.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal ball_entered(ball: Ball)
+
 const SPEED := 50.0
 
 
@@ -16,3 +18,10 @@ func _physics_process(_delta: float) -> void:
 func move(dir: Vector2) -> void:
 	dir = dir.normalized()
 	velocity = Vector3(dir.x, 0, dir.y)
+
+
+func _on_ball_area_body_entered(body: Node3D) -> void:
+	if body is not Ball:
+		return
+
+	ball_entered.emit(body)
