a31bb7468d7496d8ad3fed59d4c6d8bbf3fca02f
Jeffrey Harmon <contact@jeffreyharmon.dev>
2026-01-20 18:46:37 -0500

    Add basic scoring for hole

--- project/game/scorekeeper/scorekeeper.gd
+++ project/game/scorekeeper/scorekeeper.gd
@@ -11,7 +11,7 @@ func set_up(course: Course, players: Array[Controller]) -> void:
 	for player in players:
 		if player is BallController:
 			player.ball_hit.connect(increment_level_score.bind(player))
-		_scores[player] = LevelScores.new(course.get_levels())
+		_scores[player] = LevelScores.new(course.get_levels(), player is HoleController)
 
 
 func increment_level_score(player: Controller):
@@ -25,6 +25,8 @@ func complete_level(player: Controller = null):
 		return
 
 	for p in _scores.keys():
+		if p is HoleController:
+			_scores[p].update_hole_score(_current_level, _scores.values())
 		_scores[p].complete_level(_current_level)
 
 	_current_level += 1
@@ -41,9 +43,9 @@ class LevelScores:
 	var is_complete: Array[bool] = []
 
 
-	func _init(levels: Array[Level]) -> void:
+	func _init(levels: Array[Level], par_disabled := false) -> void:
 		for level in levels:
-			pars.append(level.get_par())
+			pars.append(0 if par_disabled else level.get_par())
 			scores.append(0)
 			is_complete.append(false)
 
@@ -52,8 +54,24 @@ class LevelScores:
 		scores[level_index] += 1
 
 
+	func update_hole_score(level_index: int, others_scores: Array[LevelScores]) -> void:
+		for other_scores in others_scores:
+			if other_scores == self:
+				continue
+
+			var other_score := other_scores.level_score(level_index)
+			if other_score < 0:
+				scores[level_index] += 1
+			elif other_score > 0:
+				scores[level_index] -= 1
+
+
 	func complete_level(level_index: int) -> void:
 		is_complete[level_index] = true
+
+
+	func level_score(level_index: int) -> int:
+		return scores[level_index] - pars[level_index]
 
 
 	func course_score() -> int:
@@ -63,6 +81,6 @@ class LevelScores:
 			if not is_complete[i]:
 				continue
 
-			total += scores[i] - pars[i]
+			total += level_score(i)
 
 		return total
