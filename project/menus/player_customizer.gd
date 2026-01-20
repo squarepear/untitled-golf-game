804741a072e8e128dbcbe00896e793a0094909ed
Jadi Miller <jadilmiller@gmail.com>
2026-01-20 17:59:19 -0500

    Create PlayerJoin

---
+++ project/menus/player_customizer.gd
@@ -0,0 +1,27 @@
+class_name PlayerCustomizer
+extends Control
+
+var _number: int
+var _current_color: Color = Color.WHITE
+@onready var _texture_rect: TextureRect = %TextureRect
+@onready var _player_label: Label = %PlayerLabel
+
+
+func _on_color_picker_color_changed(color: Color) -> void:
+	_current_color = color
+	_texture_rect.modulate = _current_color
+	PlayerInfo.player_colors[_number] = _current_color
+
+
+func set_player_name(player_number) -> void:
+	_number = int(player_number)
+	_player_label.text = "Player " + str(_number)
+
+
+func get_color() -> Color:
+	return _current_color
+
+
+func _on_button_pressed() -> void:
+	PlayerInfo.add_player()
+	
---
+++ project/menus/player_join.gd
@@ -0,0 +1,21 @@
+extends Control
+
+const PLAYER_CUSTOMIZER = preload("res://menus/player_customizer.tscn")
+@onready var _h_box_container: HBoxContainer = %HBoxContainer
+
+
+func _ready() -> void:
+	for n in PlayerInfo.players:
+		_create_player_customizer(n)
+
+
+func _on_button_pressed() -> void:
+	if PlayerInfo.players < 4:
+		PlayerInfo.add_player()
+		_create_player_customizer(PlayerInfo.players - 1)
+
+
+func _create_player_customizer(n) -> void:
+		var _player_customizer: PlayerCustomizer = PLAYER_CUSTOMIZER.instantiate()
+		_h_box_container.add_child(_player_customizer)
+		_player_customizer.set_player_name(str(n + 1))
