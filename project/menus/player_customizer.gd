f75b77bce2fe027e10f789b7e4757007d8c810ec
Jadi Miller <jadilmiller@gmail.com>
2026-02-03 19:15:23 -0500

    Stylize LineEdit in theme

--- project/menus/player_customizer.gd
+++ project/menus/player_customizer.gd
@@ -6,7 +6,7 @@ var _current_color: Color = Color.WHITE
 var _current_name: String
 @onready var _texture_rect: TextureRect = %TextureRect
 @onready var _player_label: Label = %PlayerLabel
-@onready var _text_edit: TextEdit = %TextEdit
+@onready var _line_edit: LineEdit = %LineEdit
 
 
 func _on_color_picker_color_changed(color: Color) -> void:
@@ -27,6 +27,6 @@ func get_color() -> Color:
 	return _current_color
 
 
-func _on_text_edit_text_changed() -> void:
-	_current_name = _text_edit.text
+func _on_line_edit_text_changed(new_text) -> void:
+	_current_name = new_text
 	PlayerInfo.names[_number] = _current_name
