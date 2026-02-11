674b31fb50466bd18d191b8bea634d56222f6578
Jeffrey Harmon <contact@jeffreyharmon.dev>
2026-02-10 19:12:23 -0500

    Test score increments

--- project/test/unit/score_test.gd
+++ project/test/unit/score_test.gd
@@ -11,18 +11,37 @@ func before_each() -> void:
 	scorekeeper = autoqfree(Scorekeeper.new())
 
 
-func _instantiate_course() -> Course:
-	var scene_instance: Course = add_child_autoqfree(TEST_COURSE.instantiate())
-	return scene_instance
-
-
 func test_set_up() -> void:
 	assert_not_null(course)
 	assert_not_null(scorekeeper)
 
 
 func test_score_starts_at_zero() -> void:
-	var players: Array[Controller] = [autoqfree(Controller.new())]
-	scorekeeper.set_up(course, players)
+	_set_up_scorekeeper(4)
+
 	for score in scorekeeper.get_scores():
 		assert_eq(score.scores[0], 0)
+
+
+func test_score_increment() -> void:
+	var players := _set_up_scorekeeper(4)
+
+	var scores := scorekeeper.get_scores()
+	for i in len(players):
+		assert_eq(scores[i].scores[0], 0)
+		scorekeeper.increment_level_score(players[i])
+		assert_eq(scores[i].scores[0], 1)
+
+
+func _instantiate_course() -> Course:
+	return add_child_autoqfree(TEST_COURSE.instantiate())
+
+
+func _set_up_scorekeeper(player_count := 1) -> Array[Controller]:
+	var players: Array[Controller] = []
+	for i in player_count:
+		players.append(autoqfree(Controller.new()))
+
+	scorekeeper.set_up(course, players)
+
+	return players
